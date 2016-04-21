b65596364f086c1acb96ceb8581309524414d06a
Francisco <plandino@hotmail.com>
2016-04-21 18:31:58 -0300

    Agrego los elementos y actions iniciales.

---
+++ src/main/java/ar/fiuba/tdd/tp/Juego.java
@@ -0,0 +1,8 @@
+package ar.fiuba.tdd.tp;
+
+/**
+ * Created by panchoubuntu on 21/04/16.
+ */
+public class Juego {
+
+}
---
+++ src/main/java/ar/fiuba/tdd/tp/Motor.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tdd.tp;
+
+/**
+ * Created by panchoubuntu on 21/04/16.
+ */
+public class Motor {
+}
