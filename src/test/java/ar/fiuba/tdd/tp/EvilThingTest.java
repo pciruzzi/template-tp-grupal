d4ed728b54be8df8d47b1b9dfcc35f2ebdc4189a
Fran Guerini <Fran Guerini>
2016-04-28 16:01:01 -0300

    Subo algunos tests de mas juegos

---
+++ src/test/java/ar/fiuba/tdd/tp/EvilThingTest.java
@@ -0,0 +1,9 @@
+package ar.fiuba.tdd.tp;
+
+/**
+ * Created by gg on 4/28/2016.
+ */
+public class EvilThingTest {
+
+
+}
---
+++ src/test/java/ar/fiuba/tdd/tp/HanoiTowersTest.java
@@ -0,0 +1,80 @@
+package ar.fiuba.tdd.tp;
+
+import ar.fiuba.tdd.tp.games.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by gg on 4/28/2016.
+ */
+public class HanoiTowersTest {
+/*
+    public static final String moveOneToTwo = "";
+    public static final String moveOneToThree = "";
+    public static final String moveTwoToOne = "";
+    public static final String moveTwoToThree = "";
+    public static final String moveThreeToOne = "";
+    public static final String moveThreeToTwo = "";
+    public static final String moveSuccesfull = "moved!";
+    public static final String moveFailed = "Failed";
+
+    private Game initializeGame() {
+        Game game = new FetchQuest();
+        game.createGame();
+        return game;
+    }
+
+    @Test
+    public void moveFromStackOneTooTwo() {
+        Game game = this.initializeGame();
+        assertEquals(game.doAction(moveOneToTwo),moveSuccesfull);
+    }
+
+    @Test
+    public void moveFromStackOneTooThree() {
+        Game game = this.initializeGame();
+        assertEquals(game.doAction(moveOneToThree),moveSuccesfull);
+    }
+
+    @Test
+    public void moveFromStackTwoWithoutDiskError() {
+        Game game = this.initializeGame();
+        assertEquals(game.doAction(moveTwoToOne),moveFailed);
+    }
+
+    @Test
+    public void moveFromStackOneToStackTwoTwoTimesReturnError() {
+        Game game = this.initializeGame();
+        game.doAction(moveOneToTwo);
+        assertEquals(game.doAction(moveTwoToOne),moveFailed);
+    }
+
+    @Test
+    public void moveFromStackOneToTwoAndBackToStackOne() {
+        Game game = this.initializeGame();
+        game.doAction(moveOneToTwo);
+        assertEquals(game.doAction(moveTwoToOne),moveSuccesfull);
+    }
+
+    @Test
+    public void moveTwoDiskToStackThreeInAValidMove() {
+        Game game = this.initializeGame();
+        game.doAction(moveOneToTwo);
+        game.doAction(moveOneToThree);
+        assertEquals(game.doAction(moveTwoToThree),moveSuccesfull);
+    }
+
+    @Test
+    public void cantMoveBiggerDiskOnTopOfSmallerDisk() {
+        Game game = this.initializeGame();
+        game.doAction(moveOneToTwo);
+        game.doAction(moveOneToThree);
+        assertEquals(game.doAction(moveThreeToOne),moveFailed);
+    }
+
+*/
+
+
+
+}
