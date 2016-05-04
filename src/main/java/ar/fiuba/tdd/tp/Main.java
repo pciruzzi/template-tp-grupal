d71bd4baf6cf23ae463c9d71da366574ed1218e7
flotristant <flotristant@gmail.com>
2016-05-03 21:26:58 -0300

    Cambio loader, para que utilice el path que se le pasa a la app.

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -1,11 +1,14 @@
 package ar.fiuba.tdd.tp;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         File file = new File(args[0]);
-        GameBuilder builder = BuilderLoader.load(file, Thread.currentThread().getContextClassLoader());
+        URL[] urls = { new URL("jar:file:" + args[0]+"!/") };
+        GameBuilder builder = BuilderLoader.load(file, URLClassLoader.newInstance(urls));
         builder.build();
     }
 }
