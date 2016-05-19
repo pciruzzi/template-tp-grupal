57700a6e8e1b0089de70a9defa32702e7f89626c
Pablo <p.ciruzzi@hotmail.com>
2016-05-19 15:15:53 -0300

    Si los tests corren en travis, esto deberia fallar...

---
+++ src/test/java/ar/fiuba/tdd/tp/DriverTest.java
@@ -0,0 +1,29 @@
+package ar.fiuba.tdd.tp;
+
+import ar.fiuba.tdd.tp.driver.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DriverTest {
+
+    @Test
+    public void gameDriverTest() {
+        GameDriver driver = new DriverImplementation();
+        try {
+            driver.initGame("build/classes/main/ar/fiuba/tdd/tp/model/EvilThingConfiguration.jar");
+            assertEquals(GameState.Ready, driver.getCurrentState());
+            driver.sendCommand("pick key");
+            assertEquals(GameState.InProgress, driver.getCurrentState());
+            driver.sendCommand("open door");
+            driver.sendCommand("talk to thief");
+            driver.sendCommand("open otherDoor");
+            assertEquals(GameState.Won, driver.getCurrentState());
+        } catch (GameLoadFailedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
