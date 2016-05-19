1f3e831e63b8201a9fd9a59caf0424e100d63f56
Santiago <fernandezsantid@gmail.com>
2016-05-19 20:57:01 -0300

    Actualizado el all.sh

--- all.sh
+++ all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./jars.sh -e -s -c -g FetchQuest -g EvilThing -g OpenDoor -g OpenDoor2 -g TreasureQuest -g Hanoi -g WSC
+./jars.sh -e -s -c -g FetchQuest -g EvilThing -g OpenDoor -g OpenDoor2 -g TreasureQuest -g Hanoi -g WSC -g TheEscape
