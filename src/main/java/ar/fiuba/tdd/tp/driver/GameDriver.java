aea35f308e57568aa2a31b991aa1b7dde1603106
Agustin Carames <agustin@tecnicasdedisenio.com.ar>
2016-05-19 01:07:37 -0300

    Add enum and exception to control game driver state

--- src/main/java/ar/fiuba/tdd/tp/driver/GameDriver.java
+++ src/main/java/ar/fiuba/tdd/tp/driver/GameDriver.java
@@ -1,7 +1,9 @@
 package ar.fiuba.tdd.tp.driver;
 
 public interface GameDriver {
-    void initGame(String jarPath);
+    void initGame(String jarPath) throws GameLoadFailedException;
 
     String sendCommand(String cmd);
+
+    GameState getCurrentState();
 }
---
+++ src/main/java/ar/fiuba/tdd/tp/driver/GameLoadFailedException.java
@@ -0,0 +1,4 @@
+package ar.fiuba.tdd.tp.driver;
+
+public class GameLoadFailedException extends Exception {
+}
