a075a9c277f7de3433e61966b5f5bd7066674252
Francisco <plandino@hotmail.com>
2016-06-06 15:40:09 -0300

    Cambie el nombre de un metodo del movewithcomparator por uno mas generico.

--- src/main/java/ar/fiuba/tdd/tp/icommand/MoveWithComparator.java
+++ src/main/java/ar/fiuba/tdd/tp/icommand/MoveWithComparator.java
@@ -36,7 +36,7 @@ public class MoveWithComparator extends ICommand {
 
             originElement.removeElement(movingElementOrigin);
             destinationElement.addElement(movingElementOrigin);
-            checkBiggestDiskAvailable(movingElementOrigin);
+            checkBiggestAvailable(movingElementOrigin);
             return correctMovementMessage;
         }
         return incorrectMovementMessage;
@@ -46,7 +46,10 @@ public class MoveWithComparator extends ICommand {
         return incorrectMovementMessage;
     }
 
-    private void checkBiggestDiskAvailable(Element originElement) {
+    // Esto se usa para el caso en que se necesite agarrar el ultimo disco, cuaderno o lo que sea de
+    // la pila. Solo va a estar visible cuando se hayan removido todos los otros de esa pila.
+    // TODO: Revisar con lo de multiples estados.
+    private void checkBiggestAvailable(Element originElement) {
         List<Element> elementListOrigin = originElement.getElementList();
         if ( elementListOrigin.size() == 1 ) {
             elementListOrigin.get(0).setState(true);
